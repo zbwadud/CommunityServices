0d3463ca69df9a93ea2430cce49592de5592c0f6
Nathan Smith <nathan.smith@nzqa.govt.nz>
2015-03-03 13:46:11 +1300

    WEB-7517 Modified FileService after reviewing CheckStyle warnings in Jenkins and a couple of other small changes

--- healthchecks-core/src/main/java/nz/govt/nzqa/healthchecks/resource/FileService.java
+++ healthchecks-core/src/main/java/nz/govt/nzqa/healthchecks/resource/FileService.java
@@ -7,11 +7,8 @@ package nz.govt.nzqa.healthchecks.resource;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -26,7 +23,7 @@ import jcifs.smb.SmbFile;
  * @author $Author$
  * @version $Revision$
  */
-public class FileService implements Resource {
+public final class FileService implements Resource {
 
 
     private Path filePath;
@@ -39,6 +36,7 @@ public class FileService implements Resource {
     // smb://[[[domain;]username[:password]@]server[:port]/[[share/[dir/]file]]][?[param=value[param2=value2[...]]]
 
 
+    /** {@inheritDoc} */
     public boolean ping() throws ResourceNotAvailableException {
         try {
             if (filePath != null) {
@@ -49,17 +47,14 @@ public class FileService implements Resource {
                     if (resource != null) {
                         file = new File(resource.toURI());
                         return file.exists() && file.canRead();
-                    }
-                    else {
+                    } else {
                         return false;
                     }
-                }
-                else {
+                } else {
                     File file = filePath.toFile();
                     return file.exists() && file.canRead();
                 }
-            }
-            else {
+            } else {
                 String smbPath = new StringBuilder("smb://")
                         .append(domain).append(';')
                         .append(username).append(':')
@@ -70,16 +65,9 @@ public class FileService implements Resource {
                 return smbFile.canRead();
             }
         }
-        catch (URISyntaxException|MalformedURLException|SmbException e) {
+        catch (URISyntaxException | MalformedURLException | SmbException e) {
             throw new ResourceNotAvailableException(e.getMessage(), e);
         }
-    }
-
-
-    @Override
-    public String toString() {
-
-        return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE).toString();
     }
 
 
@@ -109,5 +97,12 @@ public class FileService implements Resource {
     public FileService setPassword(String password) {
         this.password = password;
         return this;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE).toString();
     }
 }
