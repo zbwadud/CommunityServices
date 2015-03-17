a11673badda70d63f79598b1921a98135e56eb23
Nathan Smith <nathan.smith@nzqa.govt.nz>
2015-03-18 09:47:29 +1300

    WEB-7519 Consolidated health check classes into NZQAHealthCheck. Unhealthy error message injected into Health Check using spring config. Made JUnit tests pass.

--- healthchecks-core/src/main/java/nz/govt/nzqa/healthchecks/library/NZQAHealthCheck.java
+++ healthchecks-core/src/main/java/nz/govt/nzqa/healthchecks/library/NZQAHealthCheck.java
@@ -16,24 +16,28 @@ import com.codahale.metrics.health.HealthCheck;
  * @author $Author$
  * @version $Revision$
  */
-public class DatabaseHealthCheck extends HealthCheck {
+public class NZQAHealthCheck extends HealthCheck {
 
+
+    public static final String COLON_SPACE = ": ";
 
     private final Resource resource;
+    private final String errorMessage;
 
 
-    public DatabaseHealthCheck(Resource resource) {
+    public NZQAHealthCheck(Resource resource, String errorMessage) {
         this.resource = resource;
+        this.errorMessage = errorMessage;
     }
 
 
     @Override
     protected Result check() throws Exception {
         if (resource.ping()) {
-            return Result.healthy();
+            return Result.healthy(resource.toString());
         }
         else {
-            return Result.unhealthy("Cannot connect to " + resource.toString());
+            return Result.unhealthy(this.errorMessage + COLON_SPACE + resource.toString());
         }
     }
 }
