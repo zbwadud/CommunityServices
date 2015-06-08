86ac273909e8b7bc8fa7fe9c1c7e908e0a562a01
ZaidW <zaid.wadud@nzqa.govt.nz>
2015-06-08 15:46:26 +1200

    WebClient-React added

---
+++ webclient/src/main/java/com/we/communityservices/webclient/React.java
@@ -0,0 +1,43 @@
+package com.we.communityservices.webclient;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+public class React {
+
+    private NashornScriptEngine nashorn;
+
+    public React() {
+        try {
+            nashorn = (NashornScriptEngine) new ScriptEngineManager().getEngineByName("nashorn");
+            nashorn.eval(read("static/nashorn-polyfill.js"));
+            nashorn.eval(read("static/vendor/react.js"));
+            nashorn.eval(read("static/vendor/showdown.min.js"));
+            nashorn.eval(read("static/commentBox.js"));
+        }
+        catch (ScriptException e) {
+            throw new IllegalStateException("could not init nashorn", e);
+        }
+    }
+
+    public String renderCommentBox(List<Comment> comments) {
+        try {
+            Object html = nashorn.invokeFunction("renderServer", comments);
+            return String.valueOf(html);
+        }
+        catch (Exception e) {
+            throw new IllegalStateException("failed to render react component", e);
+        }
+    }
+
+    private Reader read(String path) {
+        InputStream in = getClass().getClassLoader().getResourceAsStream(path);
+        return new InputStreamReader(in);
+    }
+}
---
+++ webclient/src/test/java/com/we/communityservices/webclient/ReactTest.java
@@ -0,0 +1,33 @@
+package com.we.communityservices.webclient;
+
+import org.hamcrest.Matchers;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertThat;
+
+public class ReactTest {
+
+    @Test
+    public void testRenderCommentBox() throws Exception {
+        List<Comment> comments = new ArrayList<>();
+        comments.add(new Comment("Peter Parker", "This is a comment."));
+        comments.add(new Comment("John Doe", "This is *another* comment."));
+
+        React react = new React();
+        String html = react.renderCommentBox(comments);
+
+        assertThat(html, startsWith("<div"));
+
+        Document doc = Jsoup.parse(html);
+        assertThat(doc.select("div.comment").size(), is(2));
+    }
+
+}
